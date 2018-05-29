0cea42ef2a70b7b244fb973cb3b018f26805fa93
SvineruS <SvineruS@users.noreply.github.com>
2018-05-29 20:44:45 +0300

    блядь тест

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ admins = {
 }
 
 WEBHOOK_HOST = '77.47.130.190'
-WEBHOOK_PORT = 80  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_PORT = 443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
