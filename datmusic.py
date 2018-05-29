5a5e719fb29f591eb4df2ba889fa6b09c0be4ff4
SvineruS <SvineruS@users.noreply.github.com>
2018-05-29 18:17:37 +0300

    блядь фикс

--- datmusic.py
+++ datmusic.py
@@ -26,7 +26,9 @@ def download(url, short=False):
         if s.status_code != 200:
             print('Error: datmusic-download not 200')
             return False
-        return s.raw
+        return s.raw.read()
     except Exception as e:
         print('Error: download song!', e)
         return False
+
+print(download('https://api-2.datmusic.xyz/dl/4a5a7c17/9015bf68'))
