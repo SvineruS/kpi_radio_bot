f1ea99f44bfce176b4c30bd23f815c75a1a7ffdd
SvineruS <SvineruS@users.noreply.github.com>
2018-06-09 18:57:32 +0300

    блядь тест

--- bot.py
+++ bot.py
@@ -326,7 +326,7 @@ def query_text(inline_query):
     articles = []
     for i in range(min(5, len(music))):
         audio = music[i]
-        link = 'https://kpiradiobot.ga/music/' + '/'.join(audio['download'].split('/')[-2:])
+        link = 'http://svinua.cf/api/music/?download&s=' + '/'.join(audio['download'].split('/')[-2:])
         articles.append(
             telebot.types.InlineQueryResultAudio(i, link,
                                                  performer=audio['artist'],
