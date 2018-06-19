64813bf05cef72a21d4bf98c25b1a875fe900ffc
SvineruS <SvineruS@users.noreply.github.com>
2018-06-19 19:23:31 +0300

    + фикс

--- bot_utils.py
+++ bot_utils.py
@@ -203,11 +203,13 @@ def check_bad_words(text):
 
 
 def ban_user(id, ban_time):
-    ban_time = ban_time[1] if len(ban_time) >= 2 else 60 * 24
+    ban_time_min = ban_time[1] if len(ban_time) >= 2 else 60 * 24
+    ban_time_min = int(ban_time_min)
+    ban_time = int(ban_time * 60 + time())
     banned = read_ban()
-    banned[id] = ban_time * 60 + time()
+    banned[id] = ban_time
     write_ban(banned)
-    return int(ban_time)
+    return int(ban_time_min)
 
 def chek_ban(id):
     banned = read_ban()
