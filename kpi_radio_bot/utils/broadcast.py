b072436af23b9c57e55014339574d3d03f0da67a
svin <svinerus@gmail.com>
2019-09-09 12:37:54 +0300

    возможно смена пары названий переменных поможет предотвратить этот снежный ком хаоса..

--- kpi_radio_bot/utils/broadcast.py
+++ kpi_radio_bot/utils/broadcast.py
@@ -29,25 +29,25 @@ def is_broadcast_now(day: int, time: int) -> bool:
 
 
 async def get_broadcast_freetime(day, time):
-    break_start, break_finish = consts.broadcast_times_[day][time]
+    broadcast_start, broadcast_finish = consts.broadcast_times_[day][time]
     if is_broadcast_now(day, time):
         last_order = await radioboss.get_new_order_pos()
         if not last_order:
             return 0
-        start = last_order['time_start'].hour * 60 + last_order['time_start'].minute
+        last_order_start = last_order['time_start'].hour * 60 + last_order['time_start'].minute
     else:
         try:
             tracks_count = len(list(get_broadcast_path(day, time).iterdir()))
         except FileNotFoundError:
             tracks_count = 0
-        start = break_start + tracks_count * 3  # 3 минуты - средняя длина трека
+        last_order_start = broadcast_start + tracks_count * 3  # 3 минуты - средняя длина трека
 
-    return max(0, break_finish - start)
+    return max(0, broadcast_finish - last_order_start)
 
 
 def get_broadcast_path(day: int, time: int = False) -> Path:
     t = consts.paths['orders']
     t /= '{0} {1}'.format(day + 1, consts.times_name['week_days'][day])
-    if time is not False:  # сука 0 считается как False
+    if time is not False:  # так и должно быть
         t /= '{0} {1}'.format(time, consts.times_name['times'][time])
     return t
