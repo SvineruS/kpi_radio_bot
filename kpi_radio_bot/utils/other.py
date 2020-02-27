5b2c2cfbf2a8e37245a066569679095693a6d150
svinerus <svinerus@gmail.com>
2020-02-28 00:49:46 +0200

    road to #64

--- kpi_radio_bot/utils/other.py
+++ kpi_radio_bot/utils/other.py
@@ -4,23 +4,19 @@ from html.parser import HTMLParser
 from time import time
 
 
-class MyHTMLParser(HTMLParser):
-    data = ''
+def my_lru(maxsize=None, ttl=None):
+    def decorator(function):
+        function.cache = LRU(maxsize=maxsize, ttl=ttl)
 
-    def parse(self, data):
-        self.data = ''
-        self.feed(data)
-        return self.data
+        @functools.wraps(function)
+        async def wrapper(*args, **kwargs):
+            result = await function(*args, **kwargs)
+            function.cache[tuple(args), tuple(kwargs.items())] = result
+            return result
 
-    def handle_starttag(self, tag, attrs):
-        if tag == 'br':
-            self.data += '\n'
+        return wrapper
 
-    def handle_data(self, data):
-        self.data += data
-
-    def error(self, message):
-        pass
+    return decorator
 
 
 class LRU(OrderedDict):
@@ -50,16 +46,48 @@ class LRU(OrderedDict):
         return round(time() / self.ttl)
 
 
-def my_lru(maxsize=None, ttl=None):
-    def decorator(function):
-        function.cache = LRU(maxsize=maxsize, ttl=ttl)
+class MyHTMLParser(HTMLParser):
+    data = ''
 
-        @functools.wraps(function)
-        async def wrapper(*args, **kwargs):
-            result = await function(*args, **kwargs)
-            function.cache[tuple(args), tuple(kwargs.items())] = result
-            return result
+    def parse(self, data):
+        self.data = ''
+        self.feed(data)
+        return self.data
 
-        return wrapper
+    def handle_starttag(self, tag, attrs):
+        if tag == 'br':
+            self.data += '\n'
 
-    return decorator
+    def handle_data(self, data):
+        self.data += data
+
+    def error(self, message):
+        pass
+
+
+_hashtag_chars = "0-9,a-z," \
+    "\u00c0-\u00d6,\u00d8-\u00f6,\u00f8-\u00ff,\u0100-\u024f," \
+    "\u1e00-\u1eff,\u0400-\u0481,\u0500-\u0527,\ua640-\ua66f," \
+    "\u0e01-\u0e31,\u1100-\u11ff,\u3130-\u3185,\uA960-\uA97d," \
+    "\uAC00-\uD7A0,\uff41-\uff5a,\uff66-\uff9f,\uffa1-\uffbc"
+
+_hashtag_chars = ''.join(
+    set(
+        chr(ch_id).lower()
+        for from_ch, to_ch in
+        [
+            map(ord, range_.split('-'))
+            for range_ in _hashtag_chars.split(',')
+        ]
+        for ch_id in range(from_ch + 1, to_ch)
+    )
+)
+
+
+def id_2_hashtag(id_):
+    base = len(_hashtag_chars)
+    res = ""
+    while id_:
+        res += _hashtag_chars[int(id_ % base)]
+        x = int(id_ / base)
+    return res
