3b80b7d6274bc13671ab0aa37dee8a1df6f74e52
svinerus <svinerus@gmail.com>
2020-02-29 23:26:40 +0200

    _HASHTAG_CHARS каждый запуск был разным из-за set лол

--- kpi_radio_bot/utils/other.py
+++ kpi_radio_bot/utils/other.py
@@ -74,7 +74,7 @@ _HASHTAG_CHARS = "0-9,a-z," \
                  "\u0e01-\u0e31,\u1100-\u11ff,\u3130-\u3185,\uA960-\uA97d," \
                  "\uAC00-\uD7A0,\uff41-\uff5a,\uff66-\uff9f,\uffa1-\uffbc"
 
-_HASHTAG_CHARS = ''.join(set(
+_HASHTAG_CHARS = ''.join(sorted(set(
     chr(ch_id).lower()
     for from_ch, to_ch in
     [
@@ -82,7 +82,7 @@ _HASHTAG_CHARS = ''.join(set(
         for range_ in _HASHTAG_CHARS.split(',')
     ]
     for ch_id in range(from_ch + 1, to_ch)
-))
+)))
 
 
 def id_to_hashtag(id_: int) -> str:
