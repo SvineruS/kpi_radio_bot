6d082ffa10490ab007cc5a43e532adfd125a86b9
svinerus <svinerus@gmail.com>
2020-12-19 05:49:57 +0200

    вынес эндпоинты веб сервера из main.py

--- kpi_radio/main.py
+++ kpi_radio/main.py
@@ -1,54 +1,10 @@
 import asyncio
 import ssl
 
-from aiogram import types, Dispatcher, Bot
 from aiohttp import web
 
-import music.text
 from consts import config
-from bot.handlers import DP
-from utils import scheduler, events
-
-APP = web.Application()
-ROUTES = web.RouteTableDef()
-
-
-@ROUTES.get("/gettext/{name}")
-async def gettext(request):
-    if not (name := request.match_info.get('name')):
-        return web.Response(text="Использование: /gettext/имя_песни")
-    if not (res := await music.text.search_text(name)):
-        return web.Response(text="Ошибка поиска")
-    title, text = res
-    return web.Response(text=f"{title} \n\n{text}")
-
-
-@ROUTES.get("/playlist")
-async def history_save(request):
-    # https://HOST:PORT/playlist?artist=%artist%&title%title%&casttitle=%casttitle%&len=%seconds%&path=%path%&pass=pass
-    args = request.rel_url.query
-    if args.get('pass') != config.RADIOBOSS_DATA[2]:
-        return web.Response(text='neok')
-    fields = {
-        'artist': args.get('artist'),
-        'title': args.get('title'),
-        'casttitle': args.get('casttitle'),
-        'path': args.get('path'),
-    }
-    await events.send_history(fields)
-    return web.Response(text='ok')
-
-
-@ROUTES.post(config.WEBHOOK_PATH)
-async def webhook_handle(request):
-    update = await request.json()
-    update = types.Update(**update)
-
-    Bot.set_current(DP.bot)  # без этого не работает
-    Dispatcher.set_current(DP)
-    asyncio.create_task(DP.process_update(update))
-
-    return web.Response(text='ok')
+from utils import scheduler, events, server
 
 
 async def on_startup(_):
@@ -64,15 +20,14 @@ async def on_shutdown(_):
 
 
 def start():
-    APP.add_routes(ROUTES)
-    APP.on_startup.append(on_startup)
-    APP.on_shutdown.append(on_shutdown)
+    server.APP.on_startup.append(on_startup)
+    server.APP.on_shutdown.append(on_shutdown)
 
     context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
     context.load_cert_chain(config.SSL_CERT, config.SSL_PRIV)
 
     web.run_app(
-        APP,
+        server.APP,
         host='0.0.0.0',
         port=config.PORT,
         ssl_context=context
---
+++ kpi_radio/utils/server.py
@@ -0,0 +1,53 @@
+import asyncio
+
+from aiogram import types, Bot, Dispatcher
+from aiohttp import web
+
+from bot.handlers import DP
+from consts.config import WEBHOOK_PATH, RADIOBOSS_DATA
+from music.text import search_text
+from utils import events
+
+APP = web.Application()
+ROUTES = web.RouteTableDef()
+
+
+@ROUTES.get("/gettext/{name}")
+async def gettext(request):
+    if not (name := request.match_info.get('name')):
+        return web.Response(text="Использование: /gettext/имя_песни")
+    if not (res := await search_text(name)):
+        return web.Response(text="Ошибка поиска")
+    title, text = res
+    return web.Response(text=f"{title} \n\n{text}")
+
+
+@ROUTES.get("/playlist")
+async def history_save(request):
+    # https://HOST:PORT/playlist?artist=%artist%&title%title%&casttitle=%casttitle%&len=%seconds%&path=%path%&pass=pass
+    args = request.rel_url.query
+    if args.get('pass') != RADIOBOSS_DATA[2]:
+        return web.Response(text='neok')
+    fields = {
+        'artist': args.get('artist'),
+        'title': args.get('title'),
+        'casttitle': args.get('casttitle'),
+        'path': args.get('path'),
+    }
+    await events.send_history(fields)
+    return web.Response(text='ok')
+
+
+@ROUTES.post(WEBHOOK_PATH)
+async def webhook_handle(request):
+    update = await request.json()
+    update = types.Update(**update)
+
+    Bot.set_current(DP.bot)  # без этого не работает
+    Dispatcher.set_current(DP)
+    asyncio.create_task(DP.process_update(update))
+
+    return web.Response(text='ok')
+
+
+APP.add_routes(ROUTES)
