af8c72f4b6f157a87bcb7e03d4070967006bb0fe
svinerus <svinerus@gmail.com>
2021-02-10 13:50:39 +0200

    create tables if not exists

--- kpi_radio/utils/db/__init__.py
+++ kpi_radio/utils/db/__init__.py
@@ -1,6 +1,10 @@
+from ._connector import DB
 from .stats import Stats
 from .users import Users
 from .tracklist import Tracklist
+
+DB.create_tables([Stats, Users, Tracklist])
+
 
 __all__ = [
     'Users', 'Stats', 'Tracklist'
