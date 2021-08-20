d39abf41f164ffe69802536cdf79f79aa877fbbd
svinerus <svinerus@gmail.com>
2021-08-20 15:03:07 +0300

    Ignore all webhook errors

--- kpi_radio/bot/bot.py
+++ kpi_radio/bot/bot.py
@@ -1,6 +1,9 @@
+import logging
+
 from aiogram import Dispatcher
-from aiogram.dispatcher.webhook import get_new_configured_app
+from aiogram.dispatcher.webhook import WebhookRequestHandler, DEFAULT_WEB_PATH, DEFAULT_ROUTE_NAME, BOT_DISPATCHER_KEY
 from aiogram.utils import executor
+from aiohttp import web
 
 from bot.bot_utils import bind_filters
 from consts.config import BOT
@@ -20,5 +23,17 @@ def start_longpoll(**kwargs):
     executor.start_polling(DP, skip_updates=True, **kwargs)
 
 
+# ignore errors
+class MyHandler(WebhookRequestHandler):
+    async def process_update(self, update):
+        try:
+            return await super().process_update(update)
+        except:
+            logging.exception("WEBHOOK Exception")
+
+
 def get_aiohttp_app():
-    return get_new_configured_app(DP)
+    app = web.Application()
+    app.router.add_route('*', DEFAULT_WEB_PATH, MyHandler, name=DEFAULT_ROUTE_NAME)
+    app[BOT_DISPATCHER_KEY] = DP
+    return app
